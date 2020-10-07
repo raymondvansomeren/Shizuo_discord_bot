fec5310ec21cadc84e6ca30b383146d9081bacba
Raymond van Someren <raymondvansomeren@live.nl>
2020-10-07 20:07:47 +0200

    edits

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,27 +1,3 @@
-// module.exports.run = async (bot, message, args) =>
-// {
-//     if (!message.mentions.users.size)
-//     {
-//         return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: 'png', dynamic: true })}>`);
-//     }
-
-//     const avatarList = message.mentions.users.map(user =>
-//     {
-//         return `${user.username}'s avatar: <${user.displayAvatarURL({ format: 'png', dynamic: true })}>`;
-//     });
-
-//     // send the entire array of strings as a message
-//     // by default, discord.js will `.join()` the array with `\n`
-//     message.channel.send(avatarList);
-// };
-
-// module.exports.help =
-// {
-//     name: 'avatar',
-//     description: '', // TODO
-//     aliases: ['icon', 'pfp'],
-// };
-
 module.exports =
 {
     name: 'avatar',
--- commands/color.js
+++ commands/color.js
@@ -1,49 +1,50 @@
 const Discord = require('discord.js');
 const colors = require('../colors.json');
 
-module.exports.run = async (bot, message, args) =>
+module.exports =
 {
-    if (args.length === 0)
-    {
-        const botembed = new Discord.MessageEmbed()
-            .setTitle('Available colors')
-            .setColor('#FFDE2C')
-            .setFooter('Bot created by raymond570#2966', 'https://cdn.discordapp.com/avatars/270871921137025024/a_fd2fefc0474534a4e2ff908af79be9d1.gif');
-
-        for (const i in colors)
-        {
-            if (colors[i].color.toLowerCase() !== 'none')
-            {
-                botembed.addField(colors[i].color, '#' + colors[i].value);
-            }
-            else
-            {
-                botembed.addField(colors[i].color, colors[i].value);
-            }
-        }
-
-        message.channel.send(botembed);
-    }
-    else
-    {
-        let chosenColor = '';
-        for (const c in colors)
-        {
-            if (c.color.toLowerCase() === args[0].toLowerCase())
-            {
-                chosenColor = c.value;
-                break;
-            }
-        }
-        if (chosenColor === '')
-        {
-            console.log('Not a valid color chosen.');
-            return;
-        }
-    }
-};
-
-module.exports.help = {
     name: 'color',
     description: 'When used without any arguments, this shows all available colors. When used with (an) argument(s), it will try to assign the colored role to you (if that color exists in the bot\'s database).',
+    aliases: ['colour'],
+    execute(bot, message, args)
+    {
+        if (args.length === 0)
+        {
+            const botembed = new Discord.MessageEmbed()
+                .setTitle('Available colors')
+                .setColor('#FFDE2C')
+                .setFooter('Bot created by raymond570#2966', 'https://cdn.discordapp.com/avatars/270871921137025024/a_fd2fefc0474534a4e2ff908af79be9d1.gif');
+
+            for (const i in colors)
+            {
+                if (colors[i].color.toLowerCase() !== 'none')
+                {
+                    botembed.addField(colors[i].color, `#${colors[i].value}`);
+                }
+                else
+                {
+                    botembed.addField(colors[i].color, colors[i].value);
+                }
+            }
+
+            message.channel.send(botembed);
+        }
+        else
+        {
+            let chosenColor = '';
+            for (const c in colors)
+            {
+                if (c.color.toLowerCase() === args[0].toLowerCase())
+                {
+                    chosenColor = c.value;
+                    break;
+                }
+            }
+            if (chosenColor === '')
+            {
+                console.log('Not a valid color chosen.');
+                return;
+            }
+        }
+    },
 };
