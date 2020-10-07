6ae4f12a33184463cd6dfa5757b8a12401e475ef
Raymond van Someren <raymondvansomeren@live.nl>
2020-10-08 01:33:50 +0200

    Change to the reload command, so that only I can use it

--- commands/reload.js
+++ commands/reload.js
@@ -5,31 +5,39 @@ module.exports =
     cooldown: 10,
     execute(bot, message, args)
     {
-        if (!args.length)
+        // TODO change to var OWNER (or something like that)
+        if (message.author.id === '270871921137025024')
         {
-            return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
-        }
-        const commandName = args[0].toLowerCase();
-        const command = message.client.commands.get(commandName)
-            || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+            if (!args.length)
+            {
+                return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
+            }
+            const commandName = args[0].toLowerCase();
+            const command = message.client.commands.get(commandName)
+                || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-        if (!command)
-        {
-            return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
-        }
+            if (!command)
+            {
+                return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
+            }
 
-        delete require.cache[require.resolve(`./${command.name}.js`)];
-        try
-        {
-            const newCommand = require(`./${command.name}.js`);
-            message.client.commands.set(newCommand.name, newCommand);
-        }
-        catch (error)
-        {
-            console.error(error);
-            message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
-        }
+            delete require.cache[require.resolve(`./${command.name}.js`)];
+            try
+            {
+                const newCommand = require(`./${command.name}.js`);
+                message.client.commands.set(newCommand.name, newCommand);
+            }
+            catch (error)
+            {
+                console.error(error);
+                message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
+            }
 
-        message.channel.send(`Command \`${command.name}\` was reloaded!`);
+            message.channel.send(`Command \`${command.name}\` was reloaded!`);
+        }
+        else
+        {
+            message.channel.send(`Wait, how can you see this command. That's kinda sus, let's vote ${message.author}!`);
+        }
     },
 };
