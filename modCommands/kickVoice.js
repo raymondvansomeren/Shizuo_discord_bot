6f779ac67db140ce3c67a0557b7574ac8ecb7edf
Raymond van Someren <raymondvansomeren@live.nl>
2020-10-08 18:21:07 +0200

    added voice kick command

---
+++ modCommands/kickVoice.js
@@ -0,0 +1,50 @@
+const { modPrefix } = require('../config.json');
+
+module.exports =
+{
+    name: 'kickVoice',
+    description: 'Kick a user from a voice channel. Reason is optional.',
+    aliases: ['kv'],
+    usage: '[mention] [reason]',
+    cooldown: 3,
+    execute(bot, message, args)
+    {
+        // message.mentions.members.first().voice.kick();
+        if (message.member.hasPermission('KICK_MEMBERS'))
+        {
+            const toKick = message.mentions.members.first().voice;
+            if (message.author.id === toKick.id)
+            {
+                return message.channel.send('You can\'t kick yourself.')
+                    .then(msg =>
+                    {
+                        message.delete({ timeout: 5000 });
+                        msg.delete({ timeout: 5000 });
+                    });
+            }
+
+            if (args.length === 1)
+            {
+                toKick.kick();
+                message.channel.send(`Succesfully kicked ${toKick} from the voice channel.`)
+                    .then(msg =>
+                    {
+                        message.delete({ timeout: 5000 });
+                        msg.delete({ timeout: 5000 });
+                    });
+            }
+            else if (args.length > 1)
+            {
+                args.shift();
+                const reason = args.join(' ');
+                toKick.kick(reason);
+                message.channel.send(`Succesfully kicked ${toKick} from the voice channel with reason \`${reason}\`.`)
+                    .then(msg =>
+                    {
+                        message.delete({ timeout: 5000 });
+                        msg.delete({ timeout: 5000 });
+                    });
+            }
+        }
+    },
+};
