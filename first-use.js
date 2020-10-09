4b9ab628bcb925f33da56b2dea2849202318fa7e
Raymond van Someren <raymondvansomeren@live.nl>
2020-10-09 13:52:01 +0200

    created first time use script

---
+++ first-use.js
@@ -0,0 +1,15 @@
+// First, require the module.
+const mysql = require('better-mysql');
+
+// Then login to the mysql service
+const client = new mysql.client({
+    host: 'localhost',
+    user: 'au_bot',
+    pass: 'TYEpSVjJ4sPRSWj',
+});
+
+// Now, create the database 'better-mysql-bot', but you can rename it to whatever you want.
+client.loadDatabase('au_bot').then(async (database) =>
+{
+    const table = await client.createTable('guildsettings', ['Guild', 'Settings']);
+});
