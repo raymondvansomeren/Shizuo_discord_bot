0630b6faca3aba6f865c5e0893300067799335a1
Raymond van Someren <raymondvansomeren@live.nl>
2020-10-09 14:56:22 +0200

    edit to first-use

--- first-use.js
+++ first-use.js
@@ -1,5 +1,5 @@
 const { dbhost, dbuser, dbpassword, db } = require('./config.json');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host     : dbhost,
@@ -8,20 +8,14 @@ const connection = mysql.createConnection({
     database : db,
 });
 
-connection.connect();
-
 connection.query('CREATE TABLE guildsettings (Guild VARCHAR(255) PRIMARY KEY, Prefix VARCHAR(5) NOT NULL, ModPrefix VARCHAR(5) NOT NULL);', function (error, results, fields)
 {
     if (error)
         throw error;
-    // console.log('The solution is: ', results[0].solution);
 });
 
 connection.query('CREATE TABLE points (User VARCHAR(255) PRIMARY KEY, Guild VARCHAR(255) PRIMARY KEY, Points INT(11) UNSIGNED NOT NULL);', function (error, results, fields)
 {
     if (error)
         throw error;
-    // console.log('The solution is: ', results[0].solution);
 });
-
-connection.end();
