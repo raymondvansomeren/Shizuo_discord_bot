70c10a5800ce43fedfbf017e9b7e1af7e4ac4d6e
Raymond van Someren <raymondvansomeren@live.nl>
2020-12-01 21:08:23 +0100

    added skip command

---
+++ commands/skip.js
@@ -0,0 +1,26 @@
+const ytdl = require('ytdl-core');
+
+module.exports =
+{
+    name: 'skip',
+    description: 'Skips the current song.',
+    aliases: ['next'],
+    usage: '',
+    cooldown: 3,
+    async execute(bot, message, args)
+    {
+        serverQueue = bot.queue.get(message.guild.id);
+        if (!message.member.voice.channel)
+            return message.channel.send('You have to be in a voice channel to skip music!');
+
+        if (!serverQueue)
+            return message.channel.send('There is no song that I could skip!');
+
+        if (serverQueue.songs.length === 1)
+            message.channel.send(`No songs remaining in the queue. Disconnected from voice`);
+        else
+            message.channel.send(`Skipped **${serverQueue.songs[0].title}**`);
+
+        serverQueue.connection.dispatcher.end();
+    },
+};
