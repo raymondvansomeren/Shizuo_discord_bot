80b821977727e09c01f883314b7e43c46b59e910
Raymond van Someren <raymondvansomeren@live.nl>
2020-12-01 21:07:41 +0100

    added nowplaying command

---
+++ commands/nowplaying.js
@@ -0,0 +1,18 @@
+const ytdl = require('ytdl-core');
+
+module.exports =
+{
+    name: 'nowplaying',
+    description: 'Shows the currently playing song',
+    aliases: ['playing', 'currentsong', 'np', 'cs'],
+    usage: '',
+    cooldown: 3,
+    async execute(bot, message, args)
+    {
+        serverQueue = bot.queue.get(message.guild.id);
+        if (!serverQueue)
+            return message.channel.send('I have no songs playing right now');
+        
+        message.channel.send(`Currently playing **${serverQueue.songs[0].title}\n(${serverQueue.songs[0].url})**`);
+    },
+};
