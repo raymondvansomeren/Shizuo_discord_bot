ac26b615cb77188c93312b105860f46cab4dabed
Raymond van Someren <raymondvansomeren@live.nl>
2021-03-02 01:13:02 +0100

    Added support command

---
+++ commands/support.js
@@ -0,0 +1,12 @@
+module.exports =
+{
+    name: 'support',
+    description: 'Bot support server on discord.',
+    aliases: ['discord'],
+    usage: '',
+    cooldown: 3,
+    execute(bot, message, args)
+    {
+        message.channel.send('Here is the link to the support server: ' + require('config.json').discord);
+    },
+};
