052fadd469053bca322acf8312cb5612b465ddc6
Raymond van Someren <raymondvansomeren@live.nl>
2021-03-05 22:44:02 +0100

    avatar in botinfo changed to current

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -16,7 +16,7 @@ module.exports =
             .addField('Created On', bot.user.createdAt)
             .addField('Server amount', bot.guilds.cache.size)
             .addField('Users', bot.guilds.cache.reduce((a, g) => a + g.memberCount, 0))
-            .setFooter('Bot created by raymond570#2966', 'https://cdn.discordapp.com/avatars/270871921137025024/a_fd2fefc0474534a4e2ff908af79be9d1.gif');
+            .setFooter('Bot created by raymond570#2966', 'https://cdn.discordapp.com/avatars/270871921137025024/02ef6122a25f41e3bc1c38398e17f26e.png');
 
         // message.delete().catch(e=>{
         //     console.log('Error: ' + e);
