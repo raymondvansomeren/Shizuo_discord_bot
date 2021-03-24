6703bed189e28013dbb28161efe46c0cb900d75b
Raymond van Someren <raymondvansomeren@live.nl>
2021-03-24 19:32:07 +0100

    playpl on

--- commands/playpl.js
+++ commands/playpl.js
@@ -13,7 +13,7 @@ module.exports =
     description: 'Plays a playlist, takes a playlist id (the part in the link after "list=") or just the playlist name (but with a high chance that it won\'t work)',
     aliases: ['playlist', 'playplaylist'],
     usage: '[song url] / [search text]',
-    disabled: true,
+    disabled: false,
     async execute(bot, message, args)
     {
         if (message.member.roles.cache.find(role => role.name.toLowerCase() === 'nomusic') || message.member.roles.cache.find(role => role.name.toLowerCase() === 'incapacitated'))
