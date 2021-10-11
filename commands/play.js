ba76e9534a21e194667b25c150ed3277e6e60056
Raymond van Someren <raymondvansomeren@live.nl>
2021-10-11 20:33:39 +0200

    reverted play command seperation

--- commands/play.js
+++ commands/play.js
@@ -8,8 +8,6 @@ const { HSID, SSID, SID, SIDCC, xyoutubeidentitytoken } = require('../config.jso
 
 let now = new Date();
 
-const play = require('../modules/play.js').execute;
-
 module.exports =
 {
     name: 'play',
@@ -42,6 +40,78 @@ module.exports =
                         msg.delete({ timeout: 5000 });
                     }
                 });
+        }
+
+        function play(guild, song)
+        {
+            const serverQueue = bot.queue.get(guild.id);
+
+            if (!song)
+            {
+                serverQueue.songs = [];
+                setTimeout(() =>
+                {
+                    const serverQueue2 = bot.queue.get(guild.id);
+                    if (serverQueue2 === undefined || serverQueue2.songs.length <= 0)
+                    {
+                        message.channel.send('No more songs to play, leaving voice channel');
+                        if (serverQueue2 !== undefined)
+                        {
+                            // Leave only when there is no queue
+                            serverQueue2.voiceChannel.leave();
+                        }
+                        bot.queue.delete(guild.id);
+                    }
+                }, 15000);
+                return;
+            }
+
+            const video = ytdl(song.url,
+                {
+                    filter: 'audioonly',
+                    highWaterMark: 1 << 25, quality: 'highestaudio', requestOptions:
+                    {
+                        headers:
+                        {
+                            'Cookies': `SID=${SID}; HSID=${HSID}; SSID=${SSID}; SIDCC=${SIDCC};`,
+                            // 'cookies': `${cookies}`,
+                            // 'x-youtube-identity-token': `${xyoutubeidentitytoken}`,
+                        },
+                    },
+                });
+
+            const dispatcher = serverQueue.connection
+                .play(video,
+                    { highWaterMark: 25, plp: 5 })
+                .on('finish', () =>
+                {
+                    if (serverQueue.loop === 'all')
+                    {
+                        const temp = serverQueue.songs.shift();
+                        serverQueue.songs.push(temp);
+                    }
+                    else if (serverQueue.loop === 'single')
+                    {
+                        // do nothing
+                    }
+                    else
+                    {
+                        serverQueue.songs.shift();
+                    }
+
+                    play(guild, serverQueue.songs[0]);
+                })
+                .on('error', (error) =>
+                {
+                    now = new Date();
+                    console.error(now.toUTCString(), ':', error);
+                    serverQueue.songs.shift();
+                    serverQueue.textChannel.send(`Could not play: **${song.title}**\n<${song.url}>`);
+                    play(guild, serverQueue.songs[0]);
+                });
+
+            dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+            serverQueue.textChannel.send(`Now playing: **${song.title}**\n(${song.url})`);
         }
 
         const serverQueue = bot.queue.get(message.guild.id);
@@ -180,7 +250,7 @@ module.exports =
                 const connection = await voiceChannel.join();
                 queueContruct.connection = connection;
                 // Calling the play function to start a song
-                play(bot, message, message.guild, queueContruct.songs[0]);
+                play(message.guild, queueContruct.songs[0]);
             }
             catch (err)
             {
