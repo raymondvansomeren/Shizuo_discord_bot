8ccfce676ec684536b89533d86a0d879c93189a0
Raymond van Someren <rm.vansomeren@student.han.nl>
2022-07-30 12:06:49 +0200

    Bugfix

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,6 +1,6 @@
-const { logLevel } = require('../config.json');
-const logger = require('log4js').getLogger();
-logger.level = logLevel;
+// const { logLevel } = require('../config.json');
+// const logger = require('log4js').getLogger();
+// logger.level = logLevel;
 
 module.exports = {
     name: 'voiceStateUpdate',
@@ -8,25 +8,24 @@ module.exports = {
     execute(oldState, newState)
     {
         const client = newState.client;
-        // Update on the bots voiceState?
-        if (oldState.id === client.user.id)
+        if (client === undefined || client === null
+            || oldState.id !== client.user.id) return;
+
+        // Bot joined a channel
+        if (oldState.channelId === null && newState.channelId !== null)
         {
-            // Bot joined a channel
-            if (oldState.channelID === null && newState.channelID !== null)
-            {
-                logger.info('Joining voice channel in server:', oldState.guild.name);
-            }
-            // Bot moved to another channel
-            else if (oldState.channelID !== null && newState.channelID !== null && newState.channelID !== oldState.channelID)
-            {
-                logger.info('Moved from a voice channel in server:', oldState.guild.name);
-            }
-            // Bot leaving/kicked from a channel
-            else if (oldState.channelID !== null && newState.channelID === null)
-            {
-                logger.info('Leaving/kicked form a voice channel in server:', oldState.guild.name);
-                client.queue.delete(newState.guild.id);
-            }
+            client.logger.info('Joining voice channel in server:', oldState.guild.name);
+        }
+        // Bot moved to another channel
+        else if (oldState.channelId !== null && newState.channelId !== null && newState.channelId !== oldState.channelId)
+        {
+            client.logger.info('Moved from a voice channel in server:', oldState.guild.name);
+        }
+        // Bot leaving/kicked from a channel
+        else if (oldState.channelId !== null && newState.channelId === null)
+        {
+            client.logger.info('Leaving/kicked form a voice channel in server:', oldState.guild.name);
+            client.queue.delete(newState.guild.id);
         }
     },
 };
