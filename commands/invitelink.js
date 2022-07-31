fbcc6130cf6b26e4f478899488262c80550cf5be
Raymond van Someren <rm.vansomeren@student.han.nl>
2022-07-31 13:54:54 +0200

    added support commands

--- commands/invitelink.js
+++ commands/invitelink.js
@@ -1,13 +1,21 @@
+const baseCommand = require('../modules/base-command.js');
+const baseEmbed = require('../modules/base-embed.js');
+
 const { invitelink } = require('../config.json');
 
 module.exports =
 {
     name: 'invitelink',
     description: 'This command will return the invite link for the bot.',
-    aliases: ['invite', 'botinvite', 'link'],
-    usage: '',
-    execute(bot, message, args)
+    execute(interaction)
     {
-        message.channel.send(`Here is my link: ${invitelink}`);
+        interaction.reply({ embeds: [
+            baseEmbed.get(interaction.client)
+                .setDescription(`Invite me from [here](${invitelink})`),
+        ], ephemeral: true });
+    },
+    getCommand()
+    {
+        return baseCommand.get(this);
     },
 };
